301452d2452f94be23e9677c13d8a734dc44c9f8
Jonathan Couldridge <jcouldridge@outlook.com>
2017-04-22 07:51:18 +0100

    added game state, and inited from main

--- main.lua
+++ main.lua
@@ -1,4 +1,8 @@
-function love.load()
-    print("test")
-end
+Gamestate = require "lib.hump.gamestate"
 
+local game = require "states.game"
+
+function love.load(arg)
+    Gamestate.registerEvents()
+    Gamestate.switch(game)
+end
---
+++ states/game.lua
@@ -0,0 +1,5 @@
+return {
+    draw = function()
+        love.graphics.print("hello world", 10, 10)
+    end
+}
