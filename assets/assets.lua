75ca7260c6342a7171a66959074bf79014b04fd3
Jonathan Couldridge <jcouldridge@outlook.com>
2017-04-22 21:03:58 +0100

    some images (source and small binaries) and asset definitions

---
+++ assets/assets.lua
@@ -0,0 +1,12 @@
+return {
+    Sprites = {
+        Grass = love.graphics.newImage("/assets/jon/grass.png"),
+        Woodland = love.graphics.newImage("/assets/jon/woodland.png"),
+        Riverside = love.graphics.newImage("/assets/jon/riverside.png"),
+        Grain = love.graphics.newImage("/assets/jon/grain.png"),
+        River = love.graphics.newImage("/assets/jon/river.png"),
+        BluePlayer = {
+            Standard = love.graphics.newImage("/assets/jon/standard_blue.png")
+        }
+    }
+}
